797b5896e437dcb5c58a18aeebb71186061924f5
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2022-06-12 02:26:57 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
 
   const onButtonSubmit = () => {
     setImageUrl(input);
-    fetch("http://localhost:3000/imageurl", {
+    fetch("https://polar-journey-43583.herokuapp.com/imageurl", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -69,7 +69,7 @@ function App() {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("http://localhost:3000/image", {
+          fetch("https://polar-journey-43583.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
